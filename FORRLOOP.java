8ac44b065e363f7afce5a2038bcf3547f064409a
VenkittuS <venkatjara30@gmail.com>
2016-11-29 02:07:54 -0800

    Add files via upload

---
+++ FORRLOOP.java
@@ -0,0 +1,18 @@
+package pAYILAGAM;
+
+public class FORRLOOP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=9;i--)
+		{
+			for(int j=9;j>=i;j--)
+			{
+				System.out.print("&");
+			}
+		System.out.println("");	
+		}
+
+	}
+
+}
