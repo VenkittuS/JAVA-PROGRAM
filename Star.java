3edc8d07eb01bceb85b47a685a2c73c28b13c402
VenkittuS <venkatjara30@gmail.com>
2016-11-30 11:07:41 +0530

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,22 @@
+package pAYILAGAM;
+
+public class Star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=0;i<6;i++)
+{
+for(int j=3;j>i;j--)
+{
+	System.out.print("*");
+}
+System.out.println();
+
+for(int k=3;k<=i;k++)
+{
+System.out.print("*");
+}
+System.out.println();
+	}
+
+}}
