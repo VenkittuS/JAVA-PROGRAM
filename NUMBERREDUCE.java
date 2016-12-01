78a609069ced21153effa41a081911c6dd89224e
VenkittuS <venkatjara30@gmail.com>
2016-12-01 21:36:36 +0530

    Add files via upload

---
+++ NUMBERREDUCE.java
@@ -0,0 +1,20 @@
+package pAYILAGAM;
+
+public class NUMBERREDUCE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=0;i++){
+			for(int j=1;j>i;j--)
+			{
+				System.out.print("4\n");
+			}
+			for(int k=1;k>i;k--){
+				System.out.print("33\n");
+				System.out.print("222\n");
+				System.out.print("1111\n");
+			}
+			System.out.println();
+	}
+
+}}
---
+++ TRIANGLEE.java
@@ -0,0 +1,21 @@
+package pAYILAGAM;
+
+public class TRIANGLEE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<6;i++){
+			for(int j=3;j>i;j--){
+				System.out.print("*");
+			}
+			for(int k=3;k<=i;k++){
+			System.out.print("*");
+			}
+System.out.println();	
+	}
+
+	}
+
+	}
+
+
