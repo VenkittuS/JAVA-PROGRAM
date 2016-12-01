ed849c59c4b52ead706f380216bd166aea745cd2
VenkittuS <venkatjara30@gmail.com>
2016-12-01 15:13:18 +0530

    Add files via upload

---
+++ AscendingOrder.java
@@ -0,0 +1,22 @@
+package pAYILAGAM;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=0;i<=0;i++)
+{
+	for(int j=1;j>i;j--)
+	{
+		System.out.print("1\n");
+		System.out.println("22");
+	}
+	
+	for(int k=1;k>i;k--)
+	{
+		System.out.print("333\n");
+		System.out.println("4444");
+	
+	}System.out.println();
+	
+}}}
---
+++ DesendingOrder.java
@@ -0,0 +1,26 @@
+package pAYILAGAM;
+
+public class DesendingOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=0;i++)
+		{
+			for(int j=1;j>i;j--)
+			{
+				System.out.print("4\n");
+				System.out.println("33");
+			}
+			
+			for(int k=1;k>i;k--)
+			{
+				System.out.print("222\n");
+				System.out.println("1111");
+			
+			}System.out.println();
+			
+		}
+
+	}
+
+}
