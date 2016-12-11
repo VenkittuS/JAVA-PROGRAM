26eba95c7ca04521aa1a4ff5264f51fe6df5777d
VenkittuS <venkatjara30@gmail.com>
2016-12-11 03:02:37 -0800

    Add files via upload

---
+++ INvertTRiangle.java
@@ -0,0 +1,20 @@
+package pAYILAGAM;
+
+import java.util.Scanner;
+
+public class INvertTRiangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+
+  int num=9;
+  while(num>0){
+  for(int i=0;i<num;i++){
+	  System.out.print(+num);
+  }
+  System.out.println();
+  num--;
+	}
+
+	}}
