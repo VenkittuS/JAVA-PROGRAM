b9f8c4e11b017f1986cee58dc2f007bab90ae28d
VenkittuS <venkatjara30@gmail.com>
2016-12-11 03:15:31 -0800

    Add files via upload

---
+++ Tosscoin.java
@@ -0,0 +1,15 @@
+package pAYILAGAM;
+
+public class Tosscoin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+if(Math.random()<0.5){
+	System.out.println("heads");
+}
+else{
+	System.out.println("tails");
+}
+	}
+
+}
