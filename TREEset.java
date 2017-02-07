ec5e36d653a889ffd773f91968a85ef51dbb1bf3
VenkittuS <venkatjara30@gmail.com>
2017-02-07 17:48:56 +0530

    Add files via upload

---
+++ TREEset.java
@@ -0,0 +1,20 @@
+package COLLECTIONS;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+
+
+public class TREEset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TreeSet<Integer> ts=new TreeSet();
+		ts.add(78818);
+		ts.add(9088);
+		Iterator it=ts.iterator();
+	System.out.println(it.hasNext());	
+	}
+
+}
